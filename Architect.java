134f490d39f850ff66d59b9ecf357d4ee1deda0d
Jhn587 <jhn587@gmail.com>
2014-04-19 00:02:46 -0400

    Prelim Stuff

---
+++ Architect.java
@@ -0,0 +1,22 @@
+public class Architect {
+//a is +
+//b is ++
+//c is ++-
+//d is ++--
+//e is ++--*
+//f is ++--**
+//g is ++--***
+//h is ++--***.
+//i is ++--***..
+//j is ++--***...
+
+	private Map<Character, Deque<Character>> data;
+	private String toConvert;
+	private String toReturn;
+	
+	public Architect(String toConvert){
+		this.toConvert = toConvert;
+		
+	}
+
+}
