a6ea838584efef64fd399cc7c1e0f0b8fd35481e
Jhn587 <jhn587@gmail.com>
2014-04-19 20:47:29 -0400

    added some queues

--- Architect.java
+++ Architect.java
@@ -1,3 +1,5 @@
+import java.util.Queue;
+
 public class Architect {
 //a is +
 //b is ++
@@ -16,6 +18,84 @@ public class Architect {
 	
 	public Architect(String toConvert){
 		this.toConvert = toConvert;
+		
+		Queue<Character> forA = new Queue<>();
+		forA.add('+');
+		
+		Queue<Character> forB = new Queue<>();
+		forB.add('+');
+		forB.add('+');
+		
+		Queue<Character> forC = new Queue<>();
+		forC.add('+');
+		forC.add('+');
+		forC.add('-');
+		
+		Queue<Character> forD = new Queue<>();
+		forD.add('+');
+		forD.add('+');
+		forD.add('-');
+		forD.add('-');
+		
+		Queue<Character> forE = new Queue<>();
+		forE.add('+');
+		forE.add('+');
+		forE.add('-');
+		forE.add('-');
+		forE.add('*');
+		
+		
+		Queue<Character> forF = new Queue<>();
+		forF.add('+');
+		forF.add('+');
+		forF.add('-');
+		forF.add('-');
+		forF.add('*');
+		forF.add('*');
+
+		
+		Queue<Character> forG = new Queue<>();
+		forG.add('+');
+		forG.add('+');
+		forG.add('-');
+		forG.add('-');
+		forG.add('*');
+		forG.add('*');
+		forG.add('*');
+
+		
+		Queue<Character> forH = new Queue<>();
+		forH.add('+');
+		forH.add('+');
+		forH.add('-');
+		forH.add('-');
+		forH.add('*');
+		forH.add('*');
+		forH.add('*');
+		forH.add('.');
+		
+		
+		Queue<Character> forI = new Queue<>();
+		forI.add('+');
+		forI.add('+');
+		forI.add('-');
+		forI.add('-');
+		forI.add('*');
+		forI.add('*');
+		forI.add('*');
+		forI.add('.');
+		forI.add('.');
+		
+		
+		
+		
+		
+		
+
+
+
+
+//j is ++--***...
 		
 	}
 
