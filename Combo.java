704a0ee3e8dbd7d4b7c47b606acf2a186472c789
Jhn587 <jhn587@gmail.com>
2014-04-24 21:19:39 -0400

    changing combo

--- Combo.java
+++ Combo.java
@@ -1,14 +1,22 @@
 public class Combo{
 	private char letter;
 	private int padding;
+	private ArrayDeque<Character> toPrint;
 	
-	public Combo(char letter, char padding){
+	public Combo(char letter, char padding, ArrayDeque<Character> toCopy){
 		this.letter = letter;
 		this.padding = Integer.parseInt(("" + padding));
+		this.toPrint = new ArrayDeque<Character>(toCopy);
+		addPaddingToArray();
 	}
 
 	public String toString(){
 		return ""+this.letter+this.padding;
+	
+	}
+	
+	private void addPaddingToArray(){
+		
 	
 	}
 }
